a61fa3cc6509ade7a9804d50cfc1295b55aa233f
Tobi <noggnzzz@gmail.com>
2026-01-20 11:24:27 -0500

    sudamerica-paises

---
+++ db/category/political-geography/sudamerica-paises/feature-info.js
@@ -0,0 +1,10 @@
+var info = {
+  titulo: 'Sudamérica - Países',
+  descripcion: 'Límites internacionales de los países de Sudamérica. Incluye Argentina, Bolivia, Brasil, Chile, Colombia, Ecuador, Guyana, Paraguay, Perú, Surinam, Uruguay y Venezuela.',
+  fuente: 'IBGE 2021',
+  keyword: ['sudamerica', 'paises', 'countries', 'limite', 'frontera', 'boundaries', 'internacional', 'politico', 'ibge'],
+  categoria: 3,
+  img: 'https://raw.githubusercontent.com/1nfinit0/geospatial-repo/master/db/category/political-geography/sudamerica-paises/sudamerica-paises.png',
+  data: 'https://raw.githubusercontent.com/1nfinit0/geospatial-repo/master/db/category/political-geography/sudamerica-paises/sudamerica-paises.geojson',
+  descarga: {shp: 'https://github.com/1nfinit0/geospatial-repo/blob/master/db/category/political-geography/sudamerica-paises/sudamerica-paises.zip'}
+}
