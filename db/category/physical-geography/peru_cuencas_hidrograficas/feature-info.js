cd9d22481970f989ac99d8fa8bb9f2737ffd4ba0
Tobi <noggnzzz@gmail.com>
2026-01-27 19:22:05 -0500

    cuencas

---
+++ db/category/physical-geography/peru_cuencas_hidrograficas/feature-info.js
@@ -0,0 +1,10 @@
+var info = {
+  titulo: 'Perú - Cuencas Hidrográficas',
+  descripcion: 'Conjunto de datos que contiene la red hidrográfica del Perú, incluyendo ríos, lagos y lagunas, clasificados por cuencas hidrográficas. Esta información es esencial para estudios ambientales, planificación territorial y gestión de recursos hídricos en el país.',
+  fuente: 'INEI 2017',
+  keyword: ['peru', 'red', 'rios', 'agua', 'hidrografica', 'hidrografia', 'lagos', 'lagunas', 'cuenca'],
+  categoria: 1,
+  img: 'https://raw.githubusercontent.com/1nfinit0/geospatial-repo/master/db/category/physical-geography/peru_cuencas_hidrograficas/peru_cuencas_hidrograficas.png',
+  data: 'https://raw.githubusercontent.com/1nfinit0/geospatial-repo/master/db/category/physical-geography/peru_cuencas_hidrograficas/peru_cuencas_hidrograficas.geojson',
+  descarga: {shp: 'https://github.com/1nfinit0/geospatial-repo/blob/master/db/category/physical-geography/peru_cuencas_hidrograficas/peru_cuencas_hidrograficas.zip'}
+}
